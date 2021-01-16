5eeda15f53a47a08d2662335ab3a60cbf721c03d
Samet Ham <59627331+samet-ham@users.noreply.github.com>
2021-01-16 23:42:08 +0300

    Proje dosyası ekle.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güvenliği
+            //Do not repeat yourself
+
+            string kategoriEtiketi = "Kategoriler:";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.45;
+            double dolarBugün = 7.45;
+
+            if (dolarDun > dolarBugün)
+            {
+                Console.WriteLine("Azalış Butonu Göster");
+            }
+            else if (dolarDun < dolarBugün)
+            {
+                Console.WriteLine("Artış Butonunu Göster");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi Butonu");
+            }
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı Ayarlar Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yap Butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+        }
+    }
+}
